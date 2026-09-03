45fc4baa87957c7ce16317f723e0a40fc3784ae9
af-bsu <aidenfreeman@u.boisestate.edu>
2026-09-02 21:42:54 -0600

    Added interface and test files, started driver

--- MagicSquareDriver.java
+++ MagicSquareDriver.java
@@ -1,5 +1,39 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+
+/**
+ * Driver class to check or create magic squares.
+ * 
+ * @author Amira Freeman
+ */
 public class MagicSquareDriver {
     public static void main(String[] args) {
-        
+        // Define variables
+        boolean checkingMatrix = false;
+        boolean creatingMatrix = false;
+        File matrixFile;
+
+        // Authenticate arguments
+        switch (args[0]) {
+            case "-check" -> {
+                checkingMatrix = true;
+            }
+            case "-create" -> {
+                creatingMatrix = true;
+            }
+            default -> {
+                System.out.println("Error: Incorrect use or lack of mandatory flags");
+                System.out.println("Usage: java MagicSquareDriver <-check | -create> <filename> < |size>");
+                System.exit(1);
+            }
+        }
+
+        try {
+            
+        } catch (FileNotFoundException e) {
+            e.getMessage();
+        }
+
+        if ()
     }
 }
